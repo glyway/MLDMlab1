d1d57bf7a6e311b034142b8732e8cae1e2b4ce8c
glyway <55627397+glyway@users.noreply.github.com>
2022-02-11 15:53:59 +0400

    Initial commit

---
+++ lab1.js
@@ -0,0 +1,15 @@
+let abc, def;
+abc = prompt("Введите элементы первого множества").split(' ');
+def = prompt("Введите элементы первого множества").split(' ');
+function ob(){
+
+}
+function per(){
+
+}
+function dop(){
+
+}
+function ob(){
+
+}
