cd8e4a87f588c800209191f9fe56f5e0b654bc8b
glyway <55627397+glyway@users.noreply.github.com>
2022-02-11 16:39:26 +0400

    Update lab1.js

--- lab1.js
+++ lab1.js
@@ -1,15 +1,35 @@
 let abc, def;
-abc = prompt("Введите элементы первого множества").split(' ');
-def = prompt("Введите элементы первого множества").split(' ');
-function ob(){
-
+abc = prompt("Введите элементы первого множества");
+def = prompt("Введите элементы первого множества");
+function ob(a, b){
+    let res = a;
+    for (let e of b){
+        if (!a.includes(e)) res+=e;
+    }
+    return res;
 }
-function per(){
-
+function per(a, b){
+    let res = "";
+    for (let e of a){
+        if (b.includes(e)) res+=e;
+    }
+    return res;
 }
-function dop(){
-
+function dop(a, b){
+    let res = "";
+    for (let e of a){
+        if (!b.includes(e)) res+=e;
+    }
+    return res;
 }
-function ob(){
-
+function obe(a, b){
+    let res = "";
+    dop(a,b).split('').map((e)=>{
+        res+=e;
+    })
+    dop(b,a).split('').map((e)=>{
+        res+=e;
+    })
+    return res;
 }
+console.log(ob(abc,def), per(abc,def), dop(abc,def), dop(def,abc), obe(abc,def));
