9e2526467d0e7ae0a5ef7a8aefe0327b4af7a3c6
glyway <55627397+glyway@users.noreply.github.com>
2022-02-23 15:56:11 +0400

    UnderstoodMyTask

--- lab1.js
+++ lab1.js
@@ -1,126 +1,77 @@
+function checkm (a) {
+    if (a.length != 4) return true;
+    if (!(a[0]>='0' && a[0] <= '9' && a[1]>='0' && a[1] <= '9' && a[2]>='0' && a[2] <= '9' && a[3]>='0' && a[3] <= '9')) return true;
+    if (a[1] % 2 != 0) return true;
+    if (a[2] % 2 == 0) return true;
+    return 3;
+}
+function checkd (a) {
+    for (let i = 0; i < a.length; i++){
+        for (let j = i + 1; j < a.length; j++){
+            if (a[i] == a[j]) {
+                a.splice(j,1);
+                j--;
+            }
+            
+        }
+    }
+    return a;
+}
+
 let abc, def;
 let flag = true;
 while (flag){
     flag = false;
-    abc = prompt("Введите элементы первого множества, маска (Цифра, Чётная, Нечётная, Цифра) (набирать без пробелов друг за другом)");
-    def = prompt("Введите элементы первого множества, маска (Цифра, Чётная, Нечётная, Цифра) (набирать без пробелов друг за другом)");
-    let errabc = [], errdef = [];
-    for (let i = 0; i < abc.length; i++){
-        if (i % 4 == 0){
-            if (!(abc[i] >= '0' && abc[i] <= '9')){
-                flag = true;
-                errabc.push(i);
-            }
-        }
-        else if (i % 4 == 1){
-            if (!((abc[i] >= '0' && abc[i] <= '9') && +abc[i] % 2 == 0)){
-                flag = true;
-                errabc.push(i);
-            }
-        }
-        else if (i % 4 == 2){
-            if (!((abc[i] >= '0' && abc[i] <= '9') && +abc[i] % 2 == 1)){
-                flag = true;
-                errabc.push(i);
-            }
-        }
-        else {
-            if (!((abc[i] >= '0' && abc[i] <= '9'))){
-                flag = true;
-                errabc.push(i);
-            }
-        }
-    }
-    for (let i = 0; i < def.length; i++){
-        if (i % 4 == 0){
-            if (!(def[i] >= '0' && def[i] <= '9')){
-                flag = true;
-                errdef.push(i);
-            }
-        }
-        else if (i % 4 == 1){
-            if (!((def[i] >= '0' && def[i] <= '9') && +def[i] % 2 == 0)){
-                flag = true;
-                errdef.push(i);
-            }
-        }
-        else if (i % 4 == 2){
-            if (!((def[i] >= '0' && def[i] <= '9') && +def[i] % 2 == 1)){
-                flag = true;
-                errdef.push(i);
-            }
-        }
-        else {
-            if (!((def[i] >= '0' && def[i] <= '9'))){
-                flag = true;
-                errdef.push(i);
-            }
-        }
-    }
-    if (flag){
-        let ta = "", td = "";
-        console.log(errabc);
-        for (let e of errabc){
-            ta += e.toString() + " ";
-
-        }
-        for (let e of errdef){
-            td += e.toString() + " ";
-        }
-        alert("УПС... Вы допустили ошибочки\nВ первом множестве ошибки под индексами: " + ta + "\nВо втором ошибки под индексами: " + td);
-    }
-    let flag2 = false;
-    for (let i = 0; i < abc.length; i++){
-        for (let j = 0; j < abc.length; j++){
-            if (i != j && abc[i] == abc[j]) {
-                flag2 = true;
-                break;
-            }
-        }
-        if (flag2) break;
-    }
-    for (let i = 0; i < def.length && !flag; i++){
-        for (let j = 0; j < def.length; j++){
-            if (i != j && abc[i] == abc[j]) {
-                flag2 = true;
-                break;
-            }
-        }
-        if (flag2) break;
-    }
-    if (flag2){
-        alert("Повторения запрещены...")
-    }
+    abc = prompt("Введите элементы первого множества, маска (Цифра, Чётная, Нечётная, Цифра)");
+    def = prompt("Введите элементы первого множества, маска (Цифра, Чётная, Нечётная, Цифра)");
+    //Проверка маски
+    abc = abc.split(" ");
+    abc.map((el)=>{
+        flag = (checkm(el) == 3 ? flag : true)
+    })
+    def = def.split(" ");
+    def.map((el)=>{
+        flag = (checkm(el) == 3 ? flag : true)
+    })
+    //Проверка на дупликаты и их удаление.
+    abc = checkd(abc);
+    def = checkd(def);
+    
+    if (flag) alert("Была допущена ошибка при вводе, попробуйте ещё раз.")
 }
 function ob(a, b){
-    let res = a;
-    for (let e of b){
-        if (!a.includes(e)) res+=e;
-    }
+    let res = [...a];
+    b.map((e) => {
+        if (!a.includes(e)) res.push(e);
+    })
+    console.log(a);
     return res;
 }
 function per(a, b){
-    let res = "";
-    for (let e of a){
-        if (b.includes(e)) res+=e;
-    }
+    let res = [];
+    a.map((e) => {
+        if (b.includes(e)) res.push(e);
+    })
+    console.log(a);
     return res;
 }
 function dop(a, b){
-    let res = "";
-    for (let e of a){
-        if (!b.includes(e)) res+=e;
-    }
+    let res = [];
+    a.map((e) => {
+        if (!b.includes(e)) res.push(e);
+    })
+    console.log(a);
     return res;
 }
 function obe(a, b){
-    let res = "";
-    dop(a,b).split('').map((e)=>{
-        res+=e;
+    let res = [];
+    dop(a,b).map((e)=>{
+        res.push(e);
     })
-    dop(b,a).split('').map((e)=>{
-        res+=e;
+    dop(b,a).map((e)=>{
+        res.push(e);
     })
+    console.log(a);
     return res;
 }
-alert("A: " + abc + "\nB: " + def + "\nОбъединение: " + ob(abc,def) + "\nРазность: " + per(abc,def) + "\nДополнение A/B: " + dop(abc,def) + "\nДополнение B/A: " + dop(def,abc) + "\nСимметрическая разность: " + obe(abc,def));
+alert("A: " + abc + "\nB: " + def + "\nОбъединение: " + ob(abc,def) + "\nПересечение: " + per(abc,def) + "\nДополнение A/B: " + dop(abc,def) + "\nДополнение B/A: " + dop(def,abc) + "\nСимметрическая разность: " + obe(abc,def));
