ee8206af5b9ec909674afed8a462ffe5c2292359
glyway <55627397+glyway@users.noreply.github.com>
2022-05-06 15:45:37 +0400

    lab3Done

---
+++ lab3/lab3.js
@@ -0,0 +1,116 @@
+let relationInput;
+
+let relationAddBtn;
+let relationClearBtn;
+let relationAnalizeBtn;
+
+let relationMatrixTxt;
+
+let relationReflective;
+let relationSymmetric;
+let relationTransitive;
+let relationSkewSymmetric;
+
+let flag = true;
+
+let relationMatrix = [];
+
+let multipliedMatrix;
+
+function multiplyMatrix(arr){
+    let cloneArr = JSON.parse(JSON.stringify(arr));
+    for (let k = 0; k < arr.length; k++) {
+        for (let i = 0; i < arr.length; i++) { 
+            let t = 0;
+            for (let j = 0; j < arr.length; j++) {
+                t += arr[i][j] * arr[j][k];
+            }
+            cloneArr[i][k] = t % 2;
+       }
+    }
+    console.log(cloneArr);
+    return cloneArr;
+}
+
+function clearElements() {
+    relationInput.value = "";
+    firstSetInput.value = "";
+    secondSetInput.value = "";
+    relationMatrix = [];
+    workZone.style.display = "none";
+}
+
+
+function analizeElements() {
+    addElements();
+    let isFunction = true;
+    for (let i = 0; i < relationMatrix.length; i++){
+        let t = 0;
+        for (let j = 0; j < relationMatrix[i].length; j++){
+            t += relationMatrix[i][j];
+        }
+        if (t != 1){
+            isFunction = false;
+            break;
+        }
+    }
+
+    workZone.style.display = "block";
+    isFunctionTxt.innerHTML = isFunction ? "является функцией." : "не является функцией";
+}
+
+function addElements(){
+    let relations = relationInput.value.split(/;+\s*/).filter( e => e.trim().length > 0);
+    let firstSet = new Set(firstSetInput.value.split(/(\s+)/).filter( e => e.trim().length > 0));
+    let secondSet = new Set(secondSetInput.value.split(/(\s+)/).filter( e => e.trim().length > 0));
+    let isOkay = true;
+
+    let realtionMatrixT = new Array(firstSet.size).fill().map(() => new Array(secondSet.size));
+
+    relations = relations.map(el=>{
+        let t = el.split(/(\s+)/).filter( e => e.trim().length > 0);
+        if (t.length != 2 || !(firstSet.has(t[0]) && secondSet.has(t[1]))) isOkay = false;
+        return t;
+    })
+    
+    if(!isOkay){
+        console.log(relations)
+        alert("Была допущена ошибка при вводе, попробуйте ещё раз.");
+        return;
+    }
+
+    let i = 0, j = 0;
+    firstSet.forEach(el => {
+        secondSet.forEach(el2 => {
+            relations.forEach(el3=>{
+                if (el3[0] == el && el3[1] == el2){
+                    realtionMatrixT[i][j] = 1;
+                }
+                else if (realtionMatrixT[i][j] != 1){
+                    realtionMatrixT[i][j] = 0;
+                }
+            })
+            j++;
+        })
+        i++;
+        j = 0;
+    });
+
+    console.log(realtionMatrixT, firstSet, secondSet);
+    relationMatrix = JSON.parse(JSON.stringify(realtionMatrixT));;
+}
+
+window.onload = () => {
+    relationInput = document.getElementById("relationInput");
+    firstSetInput = document.getElementById("firstSetInput");
+    secondSetInput = document.getElementById("secondSetInput");
+    relationClearBtn = document.getElementById("relationClearBtn");
+    relationAnalizeBtn = document.getElementById("relationAnalizeBtn");
+    workZone = document.getElementById("workZone");
+    isFunctionTxt = document.getElementById("isFunctionTxt");
+
+    workZone.style.display = "none";
+
+    relationClearBtn.onclick = clearElements;
+    relationAnalizeBtn.onclick = analizeElements;
+}
